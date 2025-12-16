2d1013c96834eaf39f67a93a7c3a6a6b52a7b6f4
ShibaNoi <evergreen19832@gmail.com>
2025-12-16 23:05:44 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+//ไม่ต้องเติม function main()
+#include <iostream>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
+    if (x == 'A'){
+        x='Z';
+        return x;
+    }
+    else{
+        if(x>='B' && x<='Z'){
+            x=x-1;
+            return x;
+        }
+        else{
+            x='0';
+            return x;
+        }
+    }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
 }
